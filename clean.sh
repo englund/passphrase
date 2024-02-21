0a53afac61acb6149713d810335482fea1aba739
Victor Englund <victor.englund@gmail.com>
2024-02-21 20:42:01 +0100

    Use more correct bash

--- clean.sh
+++ clean.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 illegal_chars=-.
-has_length=(4 5)
+start_length=4
+end_length=5
 
 while IFS=$'\n' read -r line;
 do
     # Get the length of the line
     length=${#line}
 
-    if [[ "${has_length[@]}" =~ $length ]] && [[ $line != *[$illegal_chars]* ]] && [[ $line != [A-ZÅÄÖ]* ]]; then
+    if (($length >= $start_length && $length <= $end_length)) \
+        && [[ $line != *[$illegal_chars]* ]] \
+        && [[ $line != [A-ZÅÄÖ]* ]]; then
         echo "$line"
     fi
 done
