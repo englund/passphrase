1635f678232618dc082201af490aefd73ddba41e
Victor Englund <victor.englund@gmail.com>
2024-02-21 20:36:46 +0100

    Rename

--- clean.sh
+++ clean.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 illegal_chars=-.
-hasLength=(4 5)
+has_length=(4 5)
 
 while IFS=$'\n' read -r line;
 do
     # Get the length of the line
     length=${#line}
 
-    if [[ "${hasLength[@]}" =~ $length ]] && [[ $line != *[$illegal_chars]* ]] && [[ $line != [A-ZÅÄÖ]* ]]; then
+    if [[ "${has_length[@]}" =~ $length ]] && [[ $line != *[$illegal_chars]* ]] && [[ $line != [A-ZÅÄÖ]* ]]; then
         echo "$line"
     fi
 done
