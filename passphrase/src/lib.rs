51396f8fa87c5414c9b7c660cf4701355c6a25ac
Victor Englund <victor.englund@gmail.com>
2024-02-25 18:30:06 +0100

    Extract generate_passphrase to lib file

---
+++ passphrase/src/lib.rs
@@ -0,0 +1,21 @@
+
+use std::collections::HashSet;
+use rand::seq::IteratorRandom;
+
+pub fn generate_passphrase(words: &Vec<String>, num_words: u32) -> String {
+    let random_words = get_unique_random_words(words, num_words);
+    let passphrase = random_words.join(" ");
+    passphrase
+}
+
+fn get_unique_random_words(words: &Vec<String>, num_words: u32) -> Vec<String> {
+    let mut rng = rand::thread_rng();
+    let mut random_words = HashSet::new();
+
+    while random_words.len() < num_words as usize {
+        let random_word = words.iter().choose(&mut rng).unwrap().clone();
+        random_words.insert(random_word);
+    }
+
+    random_words.into_iter().collect()
+}
--- passphrase/src/main.rs
+++ passphrase/src/main.rs
@@ -1,8 +1,7 @@
-
 use clap::Parser;
-use std::{collections::HashSet, io::{self, BufRead}};
+use std::io::{self, BufRead};
 
-use rand::seq::IteratorRandom;
+use passphrase::generate_passphrase;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -31,22 +30,4 @@ fn main() {
         let passphrase = generate_passphrase(&all_words, args.length);
         println!("{}", passphrase);
     }
-}
-
-fn generate_passphrase(words: &Vec<String>, num_words: u32) -> String {
-    let random_words = get_unique_random_words(words, num_words);
-    let passphrase = random_words.join(" ");
-    passphrase
-}
-
-fn get_unique_random_words(words: &Vec<String>, num_words: u32) -> Vec<String> {
-    let mut rng = rand::thread_rng();
-    let mut random_words = HashSet::new();
-
-    while random_words.len() < num_words as usize {
-        let random_word = words.iter().choose(&mut rng).unwrap().clone();
-        random_words.insert(random_word);
-    }
-
-    random_words.into_iter().collect()
 }
